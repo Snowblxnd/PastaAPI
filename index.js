3c799f35e49a90ca0b24f14bee07f29147ba7b28
Travis Antoniello <antoniellot@yahoo.com>
2020-05-28 19:11:56 -0400

    add res.json

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const data = require('./data/pasta_data.json');
 
-app.get('/api/pasta/all', (req, res) => res.send('All pasta data here'));
+app.get('/api/pasta/all', function(req, res){
+    res.json(data);
+});
 
 app.get('/api/pasta/:id',  function(req, res){
     const id = req.params.id;
-    res.send(`Pasta with ID of ${id}`)
-
+    res.send(`Pasta with ID of ${id}`);
 });
 
-app.listen(port, () => console.log(`Pasta API listening at https://localhost:${port}`))
+app.listen(port, () => console.log(`Pasta API listening at https://localhost:${port}`));
